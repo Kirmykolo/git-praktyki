c6161b8188f9fda095b8b2e36c60bb6f0788e9c6
Jakub Piotrowski <jakubpiotrowski6637@gmail.com>
2025-05-13 11:32:58 +0200

    zmiany

--- app/src/Controller/PierwszyController.php
+++ app/src/Controller/PierwszyController.php
@@ -5,13 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Plugin\TestPlugin;
 
 class PierwszyController extends AbstractController{
     #[Route('/')]
     public function index(): Response
     {
+        $text = new TestPlugin();
+        echo $text->text();
         $contents=$this->renderView('pierwszy/index.html.twig');
         return new Response ($contents);
+
     }
 }
 
---
+++ app/src/Plugin/TestPlugin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Plugin;
+
+
+class TestPlugin{
+    public function text()
+    {
+        return "Hello from TestPlugin!";
+    }
+}
